781a94976607ebd5f4248aeb78b4300804a2508c
avpetrovska <petrovska505@gmail.com>
2026-02-17 18:10:31 +0100

    Update task1-UPD.py

--- task1-UPD.py
+++ task1-UPD.py
@@ -7,12 +7,13 @@ def get_days_from_date(date_string):
         difference = now - given_date
         return difference.days
     except ValueError:
-        print("Nice try, please enter date in YYYY-MM-DD.")
+        print("Nice try, please enter date in YYYY-MM-DD: ")
 
 def main():
-    date_string = input("Enter your date in YYYY-MM-DD format:")
+    date_string = input("Enter your date in YYYY-MM-DD format: ")
     result = get_days_from_date(date_string)
     print(result)
 
 if __name__ == "__main__":
     main()
+
