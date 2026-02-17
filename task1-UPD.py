a5ad3485a7fe2c2f95a3cfbeed5362d83aee6ad3
avpetrovska <petrovska505@gmail.com>
2026-02-17 18:09:13 +0100

    Update task1-UPD.py

--- task1-UPD.py
+++ task1-UPD.py
@@ -1,11 +1,18 @@
 from datetime import datetime
 
-
-def get_days_from_today(date : str) -> int:
+def get_days_from_date(date_string):
     try:
-        given_date = datetime.strptime(date, "%Y-%m-%d").date()
-        today = datetime.today().date()
-        delta = today - given_date
-        return delta.days
+        now = datetime.now().date()
+        given_date = datetime.strptime(date_string,"%Y-%m-%d").date()
+        difference = now - given_date
+        return difference.days
     except ValueError:
-        raise ValueError("Змініть, будь ласка, формат дати.")
+        print("Nice try, please enter date in YYYY-MM-DD.")
+
+def main():
+    date_string = input("Enter your date in YYYY-MM-DD format:")
+    result = get_days_from_date(date_string)
+    print(result)
+
+if __name__ == "__main__":
+    main()
