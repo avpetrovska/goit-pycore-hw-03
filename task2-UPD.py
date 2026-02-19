b73fd37bc914bdfefef4648d5490a9ef20dcea08
avpetrovska <petrovska505@gmail.com>
2026-02-19 17:05:12 +0100

    Update task2-UPD.py

--- task2-UPD.py
+++ task2-UPD.py
@@ -1,14 +1,13 @@
 import random
 
-
 def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
-    if min < 1 or max > 1000 or quantity < 1:
-        return []
-    if quantity > (max - min + 1):
+    if min < 1 or max > 1000 or quantity > (max - min + 1) or quantity < 1:
         return []
     numbers = random.sample(range(min, max + 1), quantity)
     return sorted(numbers)
+    
+def main():
+    print(get_numbers_ticket(1, 49, 6))
 
-
-ticket = get_numbers_ticket(1, 49, 6)
-print(ticket)
+if __name__ == "__main__":
+    main()
