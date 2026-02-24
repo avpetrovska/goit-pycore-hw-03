6d902ebd139639bb943af424f708ee0ae1a9db68
avpetrovska <petrovska505@gmail.com>
2026-02-24 10:53:06 +0100

    Update task3-UPD.py

--- task3-UPD.py
+++ task3-UPD.py
@@ -1,13 +1,13 @@
 import re
 
 def normalize_phone(phone_number : str) -> str:
+    phone = re.sub(r"[^\d+]", "", phone_number)
     if phone.startswith("+380"):
         return phone
     elif phone.startswith("380"):
         return "+" + phone
     elif phone.startswith("0"):
         return "+38" + phone
-    else:
     return phone
 
 def main():
@@ -26,3 +26,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
