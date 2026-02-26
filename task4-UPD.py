3049b68ac2720be7fcdb447bd700a89381bc989b
avpetrovska <petrovska505@gmail.com>
2026-02-26 19:35:10 +0100

    Update task4-UPD.py

--- task4-UPD.py
+++ task4-UPD.py
@@ -1,23 +1,48 @@
-from datetime import datetime, date, timedelta
+from datetime import datetime, timedelta
 
-def get_upcoming_birthdays(users : list) -> list:
+def get_upcoming_birthdays(users: list) -> list:
     today = datetime.today().date()
     upcoming_birthdays = []
+
     for user in users:
         birthday = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
-        birthday_this_year = birthday.replace(year = today.year)
+        try:
+            birthday_this_year = birthday.replace(year=today.year)
+        except ValueError:
+            birthday_this_year = birthday.replace(year=today.year, day=28)
+
         if birthday_this_year < today:
-            birthday_this_year = birthday_this_year.replace(year = today.year + 1)
+            try:
+                birthday_this_year = birthday.replace(year=today.year + 1)
+            except ValueError:
+                birthday_this_year = birthday.replace(year=today.year + 1, day=28)
+
         delta_days = (birthday_this_year - today).days
-        if delta_days >=0 and delta_days <=7:
+
+        if delta_days >= 0 and delta_days <= 7:
             congratulation_date = birthday_this_year
+
             if congratulation_date.weekday() == 5:
-                congratulation_date += timedelta(days = 2)
+                congratulation_date += timedelta(days=2)
             elif congratulation_date.weekday() == 6:
-                congratulation_date += timedelta(days = 1)
-            upcoming_birthdays.append({"name" : user["name"],\
-             "congratulation_date": congratulation_date.strftime("%Y.%m.%d")})
+                congratulation_date += timedelta(days=1)
+
+            upcoming_birthdays.append({
+                "name": user["name"],
+                "congratulation_date": congratulation_date.strftime("%Y.%m.%d")
+            })
+
     return upcoming_birthdays
 
-users = [{"name":"Birthday Gurl", "birthday":"2000.02.11"}]
-print(get_upcoming_birthdays(users))
+def main():
+    users = [
+        {"name": "John Doe", "birthday": "1985.01.23"},
+        {"name": "Jane Smith", "birthday": "1990.01.27"},
+        {"name": "Leap Year Baby Gurl", "birthday": "1992.02.29"},
+        {"name": "Birthday Gurl", "birthday": "2000.02.28"}]
+
+    upcoming_birthdays = get_upcoming_birthdays(users)
+    print("Список привітань на цьому тижні:", upcoming_birthdays)
+
+if __name__ == "__main__":
+    main()
