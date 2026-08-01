e75127e03b2d1758d319ccc9730557a14f8fb5f5
Antonina Petrovska <petrovska505@gmail.com>
2026-08-01 17:38:53 +0200

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+def get_days_from_date(date_string):
+    try:
+        now = datetime.now().date()
+        given_date = datetime.strptime(date_string,"%Y-%m-%d").date()
+        difference = now - given_date
+        return difference.days
+    except ValueError:
+        print("Nice try, please enter date in YYYY-MM-DD.")
+
+def main():
+    date_string = input("Enter your date in YYYY-MM-DD format: ")
+    result = get_days_from_date(date_string)
+    print(result)
+
+if __name__ == "__main__":
+    main()
---
+++ task3.py
@@ -0,0 +1,28 @@
+import re
+
+def normalize_phone(phone_number : str) -> str:
+    phone = re.sub(r"[^\d+]", "", phone_number)
+    if phone.startswith("+380"):
+        return phone
+    elif phone.startswith("380"):
+        return "+" + phone
+    elif phone.startswith("0"):
+        return "+38" + phone
+    return phone
+
+def main():
+    raw_numbers = [
+        "067\t123 4567",
+        "(095) 234-5678\n",
+        "+380 44 123 4567",
+        "380501234567",
+        "+38(050)123-32-34",
+        "0503451234",
+        "(050)8889900",
+        "38050-111-22-22",
+        "38050 111 22 11"]
+    sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+    print(sanitized_numbers)
+
+if __name__ == "__main__":
+    main()
